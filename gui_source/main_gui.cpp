0ce396aeb1d552e58f61c0dfb1e0f80c2c16279d
horsicq <horsicq@gmail.com>
2020-05-12 13:46:22 +0200

    Fix

--- gui_source/main_gui.cpp
+++ gui_source/main_gui.cpp
@@ -37,7 +37,7 @@ int main(int argc, char *argv[])
 
     QApplication a(argc, argv);
 
-    XOptions::adjustApplicationView();
+    XOptions::adjustApplicationView("xpeviewer.ini");
 
     GuiMainWindow w;
     w.show();
