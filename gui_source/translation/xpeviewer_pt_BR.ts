baa5b6e67a5163298d230edc9c687bcea0c8838e
horsicq <horsicq@gmail.com>
2020-05-24 00:43:55 +0200

    Fix

---
+++ gui_source/translation/xpeviewer_pt_BR.ts
@@ -0,0 +1,159 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pt_BR">
+<context>
+    <name>DialogAbout</name>
+    <message>
+        <location filename="../dialogabout.ui" line="17"/>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogabout.ui" line="93"/>
+        <source>OK</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DialogOptions</name>
+    <message>
+        <location filename="../dialogoptions.ui" line="17"/>
+        <source>Options</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogoptions.ui" line="30"/>
+        <source>Scan after open</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogoptions.ui" line="37"/>
+        <source>Save last directory</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogoptions.ui" line="44"/>
+        <source>Stay on top</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogoptions.ui" line="51"/>
+        <source>Save backup</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogoptions.ui" line="58"/>
+        <source>Style</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogoptions.ui" line="127"/>
+        <source>OK</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogoptions.ui" line="134"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogoptions.cpp" line="55"/>
+        <source>Information</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogoptions.cpp" line="55"/>
+        <source>Please restart the application</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>GuiMainWindow</name>
+    <message>
+        <location filename="../guimainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../guimainwindow.ui" line="70"/>
+        <source>File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../guimainwindow.ui" line="78"/>
+        <source>Tools</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../guimainwindow.ui" line="84"/>
+        <source>Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../guimainwindow.ui" line="95"/>
+        <source>Open</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../guimainwindow.ui" line="98"/>
+        <source>Ctrl+O</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../guimainwindow.ui" line="103"/>
+        <source>Close</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../guimainwindow.ui" line="106"/>
+        <source>Ctrl+W</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../guimainwindow.ui" line="111"/>
+        <source>Exit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../guimainwindow.ui" line="114"/>
+        <source>Ctrl+Q</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../guimainwindow.ui" line="119"/>
+        <source>Options</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../guimainwindow.ui" line="124"/>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../guimainwindow.cpp" line="72"/>
+        <source>Open file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../guimainwindow.cpp" line="72"/>
+        <source>All files</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../guimainwindow.cpp" line="149"/>
+        <location filename="../guimainwindow.cpp" line="154"/>
+        <source>Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../guimainwindow.cpp" line="149"/>
+        <source>It is not a valid PE file!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../guimainwindow.cpp" line="154"/>
+        <source>Cannot open the file!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
