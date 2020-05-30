8f88522cc5711f3e0685cf0c64eaca71319e2263
horsicq <horsicq@gmail.com>
2020-05-30 03:00:16 +0200

    Fix

--- build_winxp.bat
+++ build_winxp.bat
@@ -17,7 +17,7 @@ set ZIP_NAME=%BUILD_NAME%_%RELEASE_VERSION%
 set RES_FILE=rsrc
 
 cd build_libs
-%QT_PATH%\bin\qmake.exe build_libs.pro -r -spec win32-msvc "CONFIG+=release"
+%QT_PATH%\bin\qmake.exe build_libs.pro -r -spec %QT_SPEC% "CONFIG+=release"
 
 nmake Makefile.Release clean
 nmake
