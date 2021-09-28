eb0f45a58fef101c324028792ce16cab904b6ffd
horsicq <horsicq@gmail.com>
2021-09-28 19:04:17 +0200

    Fix: 2021-09-28

--- build_mac.sh
+++ build_mac.sh
@@ -16,28 +16,28 @@ if [ -z "$X_ERROR" ]; then
     make_translate "gui_source_tr.pro" xpeviewer
     cd "$X_SOURCE_PATH"
 
-    check_file "$X_SOURCE_PATH/build/release/xpeviewer.app/Contents/MacOS/xpeviewer"
+    check_file "$X_SOURCE_PATH/build/release/XPEViewer.app/Contents/MacOS/XPEViewer"
     if [ -z "$X_ERROR" ]; then
-        cp -R "$X_SOURCE_PATH/build/release/xpeviewer.app" "$X_SOURCE_PATH/release/$X_BUILD_NAME"
+        cp -R "$X_SOURCE_PATH/build/release/XPEViewer.app" "$X_SOURCE_PATH/release/$X_BUILD_NAME"
 
-        mkdir -p $X_SOURCE_PATH/release/$X_BUILD_NAME/xpeviewer.app/Contents/Resources/signatures
-        cp -R $X_SOURCE_PATH/signatures/crypto.db            $X_SOURCE_PATH/release/$X_BUILD_NAME/xpeviewer.app/Contents/Resources/signatures
-        cp -Rf $X_SOURCE_PATH/XStyles/qss                    $X_SOURCE_PATH/release/$X_BUILD_NAME/xpeviewer.app/Contents/Resources/
+        mkdir -p $X_SOURCE_PATH/release/$X_BUILD_NAME/XPEViewer.app/Contents/Resources/signatures
+        cp -R $X_SOURCE_PATH/signatures/crypto.db            $X_SOURCE_PATH/release/$X_BUILD_NAME/XPEViewer.app/Contents/Resources/signatures
+        cp -Rf $X_SOURCE_PATH/XStyles/qss                    $X_SOURCE_PATH/release/$X_BUILD_NAME/XPEViewer.app/Contents/Resources/
 
-        fiximport "$X_SOURCE_PATH/build/release/xpeviewer.app/Contents/MacOS/xpeviewer"
+        fiximport "$X_SOURCE_PATH/build/release/XPEViewer.app/Contents/MacOS/XPEViewer"
 
-        deploy_qt_library QtWidgets xpeviewer
-        deploy_qt_library QtGui xpeviewer
-        deploy_qt_library QtCore xpeviewer
-        deploy_qt_library QtDBus xpeviewer
-        deploy_qt_library QtPrintSupport xpeviewer
-        deploy_qt_library QtSvg xpeviewer
-        deploy_qt_library QtOpenGL xpeviewer
-        deploy_qt_library QtConcurrent xpeviewer
+        deploy_qt_library QtWidgets XPEViewer
+        deploy_qt_library QtGui XPEViewer
+        deploy_qt_library QtCore XPEViewer
+        deploy_qt_library QtDBus XPEViewer
+        deploy_qt_library QtPrintSupport XPEViewer
+        deploy_qt_library QtSvg XPEViewer
+        deploy_qt_library QtOpenGL XPEViewer
+        deploy_qt_library QtConcurrent XPEViewer
 
-        deploy_qt_plugin platforms libqcocoa xpeviewer
-        deploy_qt_plugin platforms libqminimal xpeviewer
-        deploy_qt_plugin platforms libqoffscreen xpeviewer
+        deploy_qt_plugin platforms libqcocoa XPEViewer
+        deploy_qt_plugin platforms libqminimal XPEViewer
+        deploy_qt_plugin platforms libqoffscreen XPEViewer
 
         make_release
         make_clear
