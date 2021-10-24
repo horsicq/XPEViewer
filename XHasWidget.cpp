7e5c80605242fd2efbecca9d84c39b04c5afb580
horsicq <horsicq@gmail.com>
2021-10-24 19:10:53 +0200

    Fix: 2021-10-24

--- XHasWidget.cpp
+++
@@ -1,8 +0,0 @@
-@@ -113,7 +113,7 @@ void XHashWidget::reload()
-        ui->tableWidgetRegions->setHorizontalHeaderLabels(slHeader);
-        ui->tableWidgetRegions->horizontalHeader()->setVisible(true);
-
-        for(int i=0;i<nNumberOfMemoryRecords;i++)
-        for(qint32 i=0;i<nNumberOfMemoryRecords;i++)
-        {
-            QTableWidgetItem *pItemName=new QTableWidgetItem;
