0f3169609f5d42b4e385139044e87521afc4c24f
horsicq <horsicq@gmail.com>
2022-07-20 20:09:26 +0200

    Fix: 2022-07-20

--- create_appimage.sh
+++ create_appimage.sh
@@ -14,6 +14,7 @@ cp -Rf $X_SOURCE_PATH/LINUX/hicolor/                               $X_SOURCE_PAT
 cp -Rf $X_SOURCE_PATH/XStyles/qss/                                  $X_SOURCE_PATH/release/appDir/usr/lib/xpeviewer/
 mkdir -p $X_SOURCE_PATH/release/appDir/usr/lib/xpeviewer/signatures
 cp -f $X_SOURCE_PATH/signatures/crypto.db                           $X_SOURCE_PATH/release/appDir/usr/lib/xpeviewer/signatures/
+cp -Rf $X_SOURCE_PATH/images/                                       $X_SOURCE_PATH/release/appDir/usr/lib/xpeviewer/
 
 cd $X_SOURCE_PATH/release
 
